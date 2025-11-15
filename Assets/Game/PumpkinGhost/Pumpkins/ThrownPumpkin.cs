29d823943591be3b74bb91857d4854a61f0cbf42
SparTimm <66032192+SparTimm@users.noreply.github.com>
2025-11-15 14:31:53 -0500

    Merge conflicts

--- Assets/Game/PumpkinGhost/Pumpkins/ThrownPumpkin.cs
+++ Assets/Game/PumpkinGhost/Pumpkins/ThrownPumpkin.cs
@@ -11,13 +11,21 @@ public class ThrownPumpkin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+=======
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         
+=======
+        //GetComponent<Rigidbody>().Move();
+>>>>>>> Stashed changes
     }
 
     private void OnTriggerEnter(Collider other)
