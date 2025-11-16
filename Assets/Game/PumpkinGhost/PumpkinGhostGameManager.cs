0ca23b1de0ff571059474ecc3fc7cbba8b011071
Freddie Hatch <freddiehatch@autoreg-6802278.dyn.wpi.edu>
2025-11-16 14:20:38 -0500

    Score Visualization

--- Assets/Game/PumpkinGhost/PumpkinGhostGameManager.cs
+++ Assets/Game/PumpkinGhost/PumpkinGhostGameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Game.MinigameFramework.Scripts.Framework.Input;
+using Game.MinigameFramework.Scripts.Framework.PlayerInfo;
 using TMPro;
 using UnityEngine;
 
@@ -18,20 +19,30 @@ namespace PumpkinGhost {
         // UI VARIABLES
         [Header("UI")]
         [SerializeField] private GameObject readyText;
+
         [SerializeField] private GameObject timerText;
         [SerializeField] private GameObject timerTextDraw;
         [SerializeField] private GameObject timerTextOutline;
+
+        [SerializeField] private GameObject scoreText;
+        [SerializeField] private GameObject scoreTextDraw;
+        [SerializeField] private GameObject scoreTextOutline;
         
         //SOUND
         [SerializeField] private AudioClip sound_music;
         private AudioSource _audio;
+
+        private int numPlayers;
         
 
         private void Start() {
             _audio = GetComponent<AudioSource>();
             _ranking.SetAllPlayersToRank(1); // set all players to first place
             StartCoroutine(GameTimer());
+
+            numPlayers = PlayerManager.GetNumPlayers();
         }
+
         IEnumerator GameTimer() {
             // Intro Animation
             PumpkinGhostPawn.isPawnInputEnabled = false;
@@ -43,6 +54,7 @@ namespace PumpkinGhost {
             // Timer
             timer = gameTime;
             timerText.SetActive(true);
+            scoreText.SetActive(true);
             yield return new WaitForSeconds((int)gameTime);
 
             StartCoroutine(EndMinigame());
@@ -86,12 +98,35 @@ namespace PumpkinGhost {
                         timerTextOutline.GetComponent<TextMeshProUGUI>().text = (((int) (timer * 2)) / 2f).ToString("F1");
                     }
                 }
+                scoreTextDraw.GetComponent<TextMeshProUGUI>().text = "";
+                scoreTextOutline.GetComponent<TextMeshProUGUI>().text = "";
+
+                int pnum = 0;
+                String colorText = "null";
+                foreach (int score in playerScores) {
+                    pnum += 1;
+                    if (pnum == 1)
+                        colorText = "blue";
+                    else if (pnum == 2)
+                        colorText = "red";
+                    else if (pnum == 3)
+                        colorText = "green";
+                    else
+                        colorText = "yellow";
+                    
+                    if (pnum <= numPlayers + 1) {
+                        String textAdd = "P" + (pnum).ToString() + ": " + score;
+                        scoreTextDraw.GetComponent<TextMeshProUGUI>().text += "<color=\"" + colorText + "\">" + textAdd + " </color>";
+                        scoreTextOutline.GetComponent<TextMeshProUGUI>().text += textAdd + " ";
+                    }
+                }
             }
+        
         }
         
         public void AddScore(int player, float pumpkinSize){
-            playerScores[player] += (int)(pumpkinSize * 4.0F) - 3;
-            Debug.Log(playerScores[player]);
+            playerScores[player - 1] += (int)(pumpkinSize * 4.0F) - 3;
+            Debug.Log(playerScores[player - 1]);
         }
 
         private void CalculateScores() {
